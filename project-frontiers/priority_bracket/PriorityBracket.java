fb79463ed326d24df1b299cac33ec1fcd1ed2ee4
Malec Basel <mtarabein@luc.edu>
2024-09-03 12:40:07 -0500

    Create PriorityBracket.java

---
+++ project-frontiers/priority_bracket/PriorityBracket.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+
+public class PriorityBracket {
+    
+    private ArrayList<Action> actions;
+
+    private boolean sorted;
+
+    /** Constructor */
+    public PriorityBracket() {
+        this.actions = new ArrayList<Action>(4);
+        this.sorted = true;
+    } // constructor
+
+    /** Adds an action to the back of the underlying ArrayList */
+    public void add(Action action) {
+        this.actions.add(action);
+        if(this.actions.size() > 1) {
+            this.sorted = this.sorted && this.actions.get(this.actions.size()-1).priority() >= this.actions.get(this.actions.size()-2).priority();
+            if (this.sorted = this.sorted && this.actions.get(this.actions.size()-1).priority() == this.actions.get(this.actions.size()-2).priority()) {
+                this.sorted = this.sorted && this.actions.get(this.actions.size()-1).speed() >= this.actions.get(this.actions.size()-2).speed();
+            }
+        } else {
+            this.sorted = true;
+        }
+        System.out.println("ADDED ACTION W/ SPEED VAL " + action.speed() + " && PRIO VAL " + action.priority() + "; " + (this.sorted ? "BRACKET SORTED\n" : "BRACKET NOT SORTED\n"));
+    } // method add
+
+    /** ArrayList sorter (fastest in back, slowest in front) */
+    private void sort() {
+        if(!this.actions.isEmpty()) {
+            this.sorted = false;
+            while (!this.sorted) {
+                this.sorted = true;
+                for (int i = 1; i < this.actions.size(); i++) {
+                    if (actions.get(i).priority() < this.actions.get(i-1).priority()) {
+                        Action swapper = this.actions.get(i);
+                        this.actions.set(i, this.actions.get(i-1));
+                        this.actions.set(i-1, swapper);
+                        this.sorted = false;
+                    } else if (actions.get(i).priority() == this.actions.get(i-1).priority() && actions.get(i).speed() < this.actions.get(i-1).speed()) {
+                        Action swapper = this.actions.get(i);
+                        this.actions.set(i, this.actions.get(i-1));
+                        this.actions.set(i-1, swapper);
+                        this.sorted = false;
+                    }
+                }
+                System.out.println("A ROUND OF SORTING HAS BEEN COMPLETED");
+            }
+            System.out.println("SORTING COMPLETED\n");
+        }
+    } // method sort
+
+    /** Fastest element retriever/remover method -- only works when sorted */
+    public Action pop() {
+        if (!this.sorted) {
+            this.sort();
+        }
+
+        Action result = null;
+        if (!actions.isEmpty()) {
+            int num_same = 1;
+            boolean same = (this.actions.size() >= 2 ? this.actions.get(this.actions.size()-1).speed() == this.actions.get(this.actions.size()-2).speed() && this.actions.get(this.actions.size()-1).priority() == this.actions.get(this.actions.size()-2).priority() : false);
+            while(same) {
+                num_same++;
+                same = (this.actions.size() >= num_same+1 ? this.actions.get(this.actions.size()-num_same).speed() == this.actions.get(this.actions.size()-1-num_same).speed() : false);
+            }
+            int select = (int)Math.ceil(Math.random() * num_same);
+            System.out.println((num_same > 1 ? "RANDOM" : "STATIC" ) + " DETERMINANT; Current bracket size " + (actions.size() > 9 ? actions.size() : "0" + actions.size()) + "; Equal value count " + num_same + "; Random value " + select + "; Priority level " + (actions.get(actions.size()-1).priority() < 0 ? actions.get(actions.size()-1).priority() : "+" + actions.get(actions.size()-1).priority() ) + "; Speed stat " + actions.get(actions.size()-1).speed());
+            result = this.actions.remove(this.actions.size()-select);
+        }
+        return result;
+    } // method pop
+
+    /** Emptiness accessor */
+    public boolean isEmpty() {
+        return actions.isEmpty();
+    } // method isEmpty
+
+    /** Turn kill; for effects that instantly end turn */
+    public void killTurn() {
+        this.actions = new ArrayList<Action>(4);
+        this.sorted = true;
+    } // method killTurn
+
+    // Tester; TEST PASSED
+    public static void main(String[] args) {
+        PriorityBracket bracket = new PriorityBracket();
+        bracket.add(new Action(0, 1));
+        bracket.add(new Action(-2, 8));
+        bracket.add(new Action(0, 4));
+        bracket.add(new Action(0, 2));
+        bracket.add(new Action(0, 5));
+
+        bracket.add(new Action(2, 1));
+        bracket.add(new Action(1, 4));
+        bracket.add(new Action(4, 2));
+        bracket.add(new Action(-5, 5));
+        bracket.add(new Action(7, 3));
+        bracket.add(new Action(1, 4));
+        bracket.add(new Action(2, 2));
+        bracket.add(new Action(2, 3));
+        bracket.add(new Action(4, 4));
+
+        bracket.add(new Action(0, 3));
+        bracket.add(new Action(0, 4));
+        bracket.add(new Action(0, 2));
+        bracket.add(new Action(0, 3));
+        bracket.add(new Action(0, 4));
+
+        boolean result = true;
+        Action lastElement = bracket.pop();
+
+        while(!bracket.isEmpty()) {
+            Action currentElement = bracket.pop();
+            if (currentElement.priority() != lastElement.priority()) {
+                result = result && currentElement.priority() <= lastElement.priority();
+            } else {
+                result = result && currentElement.speed() <= lastElement.speed();
+            }
+            lastElement = currentElement;
+        }
+        System.out.println("\nTEST RESULT: " + (result ? "passed" : "failed"));
+    }
+}
