a728d78d67964cf5c5879c82b3741a813b77080b
Malec Basel <mtarabein@luc.edu>
2024-10-04 15:08:07 -0500

    Update Solution.java
    optimized code

--- leetcode/0649-dota2-senate/Solution.java
+++ leetcode/0649-dota2-senate/Solution.java
@@ -1,39 +1,56 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        int prevSize = senate.length()+1;
         int rBanned = 0;
         int dBanned = 0;
+        int numD = 0;
+        int numR = 0;
         Queue<Character> queue = new LinkedList<Character>();
 
-        for (char c : senate.toCharArray())
-            queue.offer(c);
-
-        while(queue.size() < prevSize) {
-            prevSize = queue.size();
-            for(int i = 0; i < prevSize; i++) {
-                char curr = queue.poll();
-
-                if(curr == 'R') {
-                    if (dBanned > 0) {
-                        dBanned--;
-                    } else {
-                        queue.add(curr);
-                        rBanned++;
-                    }
+        for (char curr : senate.toCharArray()) {
+            if(curr == 'R') {
+                if (dBanned > 0) {
+                    dBanned--;
                 } else {
-                    if (rBanned > 0) {
-                        rBanned--;
-                    } else {
-                        queue.add(curr);
-                        dBanned++;
-                    }
+                    queue.add(curr);
+                    rBanned++;
+                    numR++;
+                }
+            } else {
+                if (rBanned > 0) {
+                    rBanned--;
+                } else {
+                    queue.add(curr);
+                    dBanned++;
+                    numD++;
+                }
+            }
+        }
+
+        while(Math.min(numD, numR) > 0) {
+            if(queue.peek() == 'R') {
+                if (dBanned > 0) {
+                    dBanned--;
+                    numR--;
+                    queue.poll();
+                } else {
+                    queue.add(queue.poll());
+                    rBanned++;
+                }
+            } else {
+                if (rBanned > 0) {
+                    rBanned--;
+                    numD--;
+                    queue.poll();
+                } else {
+                    queue.add(queue.poll());
+                    dBanned++;
                 }
             }
         }
 
         String result;
 
-        if(dBanned == 0) {
+        if(numR > 0) {
             result = "Radiant";
         } else {
             result = "Dire";
